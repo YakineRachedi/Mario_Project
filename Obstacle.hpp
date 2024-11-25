1c0c0ab6e4c548c2e885d4ce83c01a921b323373
Sarah Berkane <sarahkeita@Ordinateur-portable-de-Sarah.local>
2024-11-25 14:31:18 +0100

    Classe Obstacle début

---
+++ Obstacle.hpp
@@ -0,0 +1,24 @@
+#ifndef OBSTACLE_CLASS
+#define OBSTACLE_CLASS
+#include "MARIO.hpp"
+
+
+class Obstacle{
+    private:
+        // position en x et y, largeur et hauteur de l'obstacle
+        double x, y, h, l;
+        friend class Screen; // Permet à Screen d'accéder aux membres privés de Mario
+
+    public:
+        // méthode contacte à faire soit ici soit dans la classe Mario !
+        Obstacle(double _x, double _y, double _h, double _l) : x(_x), y(_y), h(_h), l(_l) {}
+
+        // getters
+        double get_position_x() const {return this->x;}
+        double get_position_y() const {return this->y;}
+        double get_hauteur() const {return this->h;}
+        double get_largeur() const {return this->l;} 
+
+        //double distance(double position_mario_x, double position_mario_y);
+
+};
